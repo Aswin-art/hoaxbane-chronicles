b2d48f16d13362367ddf76ab02b5991f1824aaf3
Aswin-art <aswinarung2@gmail.com>
2024-06-16 15:33:06 +0700

    feat: add custom loading ui

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ const k = kaboom({
   height: window.innerHeight,
   letterbox: true,
   global: false,
+  loadingScreen: false,
+  fullscreen: true,
 });
 
 // memuat font
@@ -235,5 +237,35 @@ const scenes = {
 for (const sceneName in scenes) {
   k.scene(sceneName, () => scenes[sceneName](k));
 }
+
+k.load(
+  new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, 1000);
+  })
+);
+
+k.onLoading((progress) => {
+  k.drawRect({
+    width: k.width(),
+    height: k.height(),
+    color: k.rgb(0, 0, 0),
+  });
+
+  k.drawCircle({
+    pos: k.center(),
+    radius: 32,
+    end: k.map(progress, 0, 1, 0, 360),
+  });
+
+  k.drawText({
+    text: "loading" + ".".repeat(k.wave(1, 4, k.time() * 12)),
+    font: "monospace",
+    size: 24,
+    anchor: "center",
+    pos: k.center().add(0, 70),
+  });
+});
 
 k.go("menu");
